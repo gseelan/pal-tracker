5a126678912a91e29f243f470eff851907d26579
gseelan <gnanaseelan.m@gmail.com>
2020-02-22 17:42:22 +0530

    My JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,5 +1,9 @@
 package io.pivotal.pal.tracker;
 
+import java.util.TimeZone;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +11,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class PalTrackerApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(PalTrackerApplication.class, args);
 	}
 	
 	@Bean
-	public TimeEntryRepository getTimeEntryRepo() {
-		return new InMemoryTimeEntryRepository();
+	public TimeEntryRepository getTimeEntryRepo(DataSource dataSource) {
+		return new JdbcTimeEntryRepository(dataSource);
 	}
 }
